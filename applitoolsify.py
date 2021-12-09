a903a7961de4a0ca5c1701bf1dd77b2303f3d2c9
Serhii Khalymon <sergiykhalimon@gmail.com>
2021-12-09 15:33:40 +0200

    Refactor code

--- applitoolsify.py
+++ applitoolsify.py
@@ -1,5 +1,6 @@
 from __future__ import print_function, unicode_literals
 
+import os
 import sys
 import time
 from contextlib import contextmanager
@@ -21,25 +22,21 @@ if PY2:
 else:
     from urllib.request import urlopen
 
-import os
 
-INSTRUMENTED_URL = (
-    "https://raw.githubusercontent.com/applitools/applitoolsify/main/src/instrument.py"
-)
-
-
-def _local_run():
+def _run_from_local():
     from src.instrument import run
 
     return run
 
 
-def _remote_run():
+def _run_from_remote():
+    instrumented_url = "https://raw.githubusercontent.com/applitools/applitoolsify/main/src/instrument.py"
+
     instrument_module = "_{}_instrument".format(str(time.time()).split(".")[0])
     cur_dir = sys.path[0]
     instrument_path = os.path.join(cur_dir, "{}.py".format(instrument_module))
     try:
-        with urlopen(INSTRUMENTED_URL) as resp:
+        with urlopen(instrumented_url) as resp:
             if resp.code != 200:
                 print("! Failed to download script")
                 sys.exit(1)
@@ -63,15 +60,15 @@ def _remote_run():
 
 
 @contextmanager
-def instrumenter(local_run=False):
-    if local_run:
-        yield _local_run()
+def _instrumenter(debug_with_local_run=False):
+    if debug_with_local_run:
+        yield _run_from_local()
     else:
-        yield _remote_run()
+        yield _run_from_remote()
 
 
 def main():
-    with instrumenter() as run:
+    with _instrumenter(debug_with_local_run=False) as run:
         run()
 
 
