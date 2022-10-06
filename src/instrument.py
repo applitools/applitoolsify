adc1a48b8dd110d6822cee109ccb6e1b176d552a
Ishay Peled <ishaypeled@ishaypeled.com>
2022-10-06 17:30:02 +0300

    MM

--- src/instrument.py
+++ src/instrument.py
@@ -69,7 +69,7 @@ SUPPORTED_FRAMEWORKS = {
     SdkParams.android_nmg: SdkData(
         **{
             "name": "NMG_lib",
-            "download_url": "https://applitools.jfrog.io/artifactory/nmg/android/instrumentation/NMG_lib-g8bcc399.zip",
+            "download_url": "https://applitools.jfrog.io/artifactory/nmg/android/instrumentation/NMG_lib-no-gradle.zip",
         }
     ),
     SdkParams.ios_nmg: SdkData(
