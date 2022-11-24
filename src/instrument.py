8d166c633a0daa509eefac682d21cb3acb545c68
Ishay Peled <ishaypeled@ishaypeled.com>
2022-11-24 13:28:35 +0200

    Update to version 1.0.102

--- src/instrument.py
+++ src/instrument.py
@@ -69,7 +69,7 @@ SUPPORTED_FRAMEWORKS = {
     SdkParams.android_nmg: SdkData(
         **{
             "name": "NMG_lib",
-            "download_url": "https://applitools.jfrog.io/artifactory/nmg/android/instrumentation/NMG_lib-1.0.100-g4a58975.zip",
+            "download_url": "https://applitools.jfrog.io/artifactory/nmg/android/instrumentation/NMG_lib-1.0.102-g52747fd.zip",
         }
     ),
     SdkParams.ios_nmg: SdkData(
