00705cbd955dd8e47b501f8a0b143c8f24188d17
Ishay Peled <ishaypeled@ishaypeled.com>
2022-12-04 21:16:05 +0200

    Use local by default

--- src/instrument.py
+++ src/instrument.py
@@ -69,7 +69,7 @@ SUPPORTED_FRAMEWORKS = {
     SdkParams.android_nmg: SdkData(
         **{
             "name": "NMG_lib",
-            "download_url": "https://applitools.jfrog.io/artifactory/nmg/android/instrumentation/NMG_lib-1.0.103-gc88c97c.zip",
+            "download_url": "https://applitools.jfrog.io/artifactory/nmg/android/instrumentation/NMG_lib-1.0.103-g553e6d5.zip",
         }
     ),
     SdkParams.ios_nmg: SdkData(
